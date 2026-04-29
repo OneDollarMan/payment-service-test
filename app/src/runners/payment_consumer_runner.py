eb45ee1e3d5246626a1d89d50211e81ddb9ea74f
Skorikov Vladislav <skorikov@sputnik.llc>
2026-04-29 11:26:12 +0400

    build_payment_consumer_service

--- app/src/runners/payment_consumer_runner.py
+++ app/src/runners/payment_consumer_runner.py
@@ -1,4 +1,7 @@
 import asyncio
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from src.broker import PaymentCreatedEvent
 from src.services.payment_service import PaymentService
 from src.repositories.outbox_repository import OutboxMessageRepository
@@ -9,16 +12,22 @@ from src.runners.base_broker_runner import BaseBrokerRunner
 from src.services.payment_consumer_service import PaymentConsumerService
 
 
+def build_payment_consumer_service(session: AsyncSession) -> PaymentConsumerService:
+    payment_repository = PaymentRepository()
+    outbox_repository = OutboxMessageRepository()
+
+    payment_service = PaymentService(
+        session=session,
+        payment_repository=payment_repository,
+        outbox_repository=outbox_repository,
+    )
+    return PaymentConsumerService(payment_service=payment_service)
+
+
 @broker.subscriber(payments_new_queue, no_reply=True)
 async def consume_payment_created(event: PaymentCreatedEvent) -> None:
     async with async_session_maker() as session:
-        payment_consumer_service = PaymentConsumerService(
-            payment_service=PaymentService(
-                session=session,
-                payment_repository=PaymentRepository(),
-                outbox_repository=OutboxMessageRepository(),
-            )
-        )
+        payment_consumer_service = build_payment_consumer_service(session)
         await payment_consumer_service.handle_payment_created(event)
 
 
